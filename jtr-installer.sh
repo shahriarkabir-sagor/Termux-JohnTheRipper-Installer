4c79bb6815f858cad248e9d99c4582898883a4c0
Shahriar Kabir Sagor <75204507+azclub-ltd@users.noreply.github.com>
2022-04-15 20:24:09 +0600

    update jtr-installer.sh

--- jtr-installer.sh
+++ jtr-installer.sh
@@ -9,15 +9,15 @@ BLUE='\033[0;34m'
 LIGHTBLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
-echo -e "${YELLOW}Checking if all package are updated or not ${NC}" &&
-pkg upgrade -y &&
-echo -e "${GREEN}Installing dependencies${NC}" &&
-pkg install git clang perl build-essential make -y &&
-echo -e "${GREEN}Started cloning core project${NC}" &&
-git clone git://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo john &&
-echo -e "${BLUE}Navigating to the main folder${NC}" &&
-cd john/src/ &&
-clear &&
+echo -e "${YELLOW}Checking if all package are updated or not ${NC}"
+pkg up -y
+echo -e "${GREEN}Installing dependencies${NC}"
+pkg install git clang perl build-essential make -y
+echo -e "${GREEN}Started cloning core project${NC}"
+git clone git://github.com/magnumripper/JohnTheRipper -b bleeding-jumbo john
+echo -e "${BLUE}Navigating to the main folder${NC}"
+cd john/src/
+clear
 echo -e "
 ${BLUE}**********************************************
 *${NC}${LIGHTGREEN}    _________.__             ____  __.__    ${BLUE}*
@@ -31,12 +31,13 @@ ${BLUE}**********************************************
 *${NC}${LIGHTBLUE}  johnTheRipper installer by Encoder's YT   ${BLUE}*
 **********************************************${NC}
 "
-echo -e "${GREEN}Started configuration, It may took some time${NC}" &&
-echo -e "${GREEN}Configuring core engine, please wait ...${NC}" &&
-./configure &&
-clear &&
+echo -e "${GREEN}Started configuration, It may took some time${NC}"
+sleep 0.1
+echo -e "${GREEN}Configuring core engine, please wait ...${NC}"
+./configure
+clear
 echo -e "${GREEN}Configuration Completed. Now starting final proccess it will took approximately 5-10 min"
-echo -e "Started final process, please wait...${NC}" &&
-make -s clean && make -sj4 &&
-cd ../../ &&
+echo -e "Started final process, please wait...${NC}"
+make -s clean && make -sj4
+cd ../../
 echo -e "${GREEN}Process completed. You can check it by giving the following command ${LIGHTBLUE}./john/run/john --help${NC}"
